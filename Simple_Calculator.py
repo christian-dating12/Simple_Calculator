c62867613115e4deed5d2b546b1069b1d194e35b
chrismdating1206@gmail.com <christian.dating12>
2023-05-12 22:21:27 +0800

    intro

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -10,6 +10,19 @@
 # 7. Use Python Function and appropriate Exeptions to capture errors during runtime.
 
 # PSEUDOCODE
+
+#Intro
+import pyfiglet
+
+greet = "GOOD DAY!"
+print("\033[92m" + pyfiglet.figlet_format(greet, font = "puffy"))
+input("\033[94mPress Any Key to Start...")
+print("\n")
+
+print("\033[90m=" *100)
+print("\033[93m SIMPLE CALCULATOR \033[0m".center(105))
+print("\033[90m=" *100) 
+
 # Ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication, Division)
 # Ask the user for two inputs
 # Display the result
